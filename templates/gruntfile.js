ebd042022f661eca6c6d6e6d2e32907fb0079e7c
djfrsn <dj@dennisjefferson.com>
2014-04-10 12:36:36 -0700

    Changes

---
+++ templates/gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+  css: {
+    files: ['**/*.scss', '**/*.html'],
+    tasks: ['sass'],
+    options: {
+      livereload: 35739,
+    },
+  },
+},
+sass: {
+        dist: {
+            files: {
+                'css/paint.css': 'scss/paint.scss'
+            }
+        }
+    }
+});
+  // init here
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  // this would be run by typing "grunt test" on the command line
+  grunt.registerTask('test', ['watch']);
+  
+  // the default task can be run just by typing "grunt" on the command line
+  grunt.registerTask('default', ['sass']);
+};
